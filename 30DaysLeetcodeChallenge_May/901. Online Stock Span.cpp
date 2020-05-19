4bb12e9015afca32603a6db0828c5785fecd904c
Siddhant Bhisikar <siddhantb99@gmail.com>
2020-05-19 16:29:22 +0530

    Add files via upload

---
+++ 30DaysLeetcodeChallenge_May/901. Online Stock Span.cpp
@@ -0,0 +1,62 @@
+/*
+Online Stock Span
+Solution
+Write a class StockSpanner which collects daily price quotes for some stock, and returns the span of that stock's price for the current day.
+The span of the stock's price today is defined as the maximum number of consecutive days (starting from today and going backwards) for which the price of the stock was less than or equal to today's price.
+For example, if the price of a stock over the next 7 days were [100, 80, 60, 70, 60, 75, 85], then the stock spans would be [1, 1, 1, 2, 1, 4, 6].
+
+Example 1:
+
+Input: ["StockSpanner","next","next","next","next","next","next","next"], [[],[100],[80],[60],[70],[60],[75],[85]]
+Output: [null,1,1,1,2,1,4,6]
+Explanation: 
+First, S = StockSpanner() is initialized.  Then:
+S.next(100) is called and returns 1,
+S.next(80) is called and returns 1,
+S.next(60) is called and returns 1,
+S.next(70) is called and returns 2,
+S.next(60) is called and returns 1,
+S.next(75) is called and returns 4,
+S.next(85) is called and returns 6.
+
+Note that (for example) S.next(75) returned 4, because the last 4 prices
+(including today's price of 75) were less than or equal to today's price.
+*/
+
+class StockSpanner {
+public:
+    stack<pair<int, int>> s;
+    StockSpanner() {
+        
+    }
+    
+    int next(int price) {
+        int eles = 1;
+        if(s.empty()){
+            s.push(make_pair(price, eles));
+            return eles;
+        }
+        else{
+            auto l = s.top();
+            while(l.first <= price){
+                eles += l.second;
+                s.pop();
+                if(!s.empty())
+                    l = s.top();
+                else{
+                    s.push(make_pair(price, eles));
+                    return eles;
+                }
+
+            }
+            s.push(make_pair(price, eles));
+            return eles;
+        }
+    }
+};
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner* obj = new StockSpanner();
+ * int param_1 = obj->next(price);
+ */
